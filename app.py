54a6a08f1d12f672bc949ffe0cdbdd6828e0fb9d
DongwooKim <kimphysicsman@gmail.com>
2022-05-19 13:24:17 +0900

    최초 커밋

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect, url_for, render_template, jsonify, request
+
+
+app = Flask(__name__)
+
+# HTML 화면 보여주기
+@app.route('/')
+def home():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
