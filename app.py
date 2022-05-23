c49df4b31a1d6131f09d1f4c614da24ac1a41960
creamone <gsukim777@gmail.com>
2022-05-23 20:45:51 +0900

    기능분할!

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
-from flask import Flask, redirect, url_for, render_template, jsonify, request
+from flask import Flask, render_template, request, jsonify
+from datetime import datetime
+from functions import upload
 
+import tensorflow as tf
+import numpy as np
+import os
 
+# 학습시킨 모델 불러오기 (0이면 고양이, 1이면 강아지)
+# model = tf.keras.models.load_model('static/model/model.h5')
 app = Flask(__name__)
-
-# HTML 화면 보여주기
+# cors = CORS(app, resources={r'*': {'origins': '*'}})
 @app.route('/')
 def home():
-    return 'hello world'
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload.():
+    upload.save_user_img()
+
+    file = request.files['user_img']
+    title = request.form['title_give']
+    extension = file.filename.split('.')[-1]
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    filename = f'{mytime}'
+    save_to = f'static/images/user/{title}_{filename}.{extension}'
+    file.save(save_to)
+
+    return jsonify({'result':'success'})
+
+# @app.route('/search', methods=['POST'])
+# def search():
+#     title = request.form['title_give']
+#     filenames = os.listdir('static/img/catdog')
+#     matched_files = ['static/img/catdog/'+filename for filename in filenames if title in filename]
+#     result_dict = []
+#     for index, matched_file in enumerate(matched_files):
+#         image = tf.keras.preprocessing.image.load_img(matched_file, target_size=(256, 256))
+#         input_arr = tf.keras.preprocessing.image.img_to_array(image)
+#         input_arr = np.array([input_arr])
+#         predictions = model.predict(input_arr)
+#         if predictions[0][0] > 0.5:
+#             result = '강아지'
+#         else:
+#             result = '고양이'
+#         result_dict.append({'index': index, 'path':matched_file, 'result':result})
+#     return jsonify({'predictions': result_dict})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
